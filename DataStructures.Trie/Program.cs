9c4f7d9f90b8f5fcb17ba41277eeb035744ae707
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-09-27 18:55:05 +0300

    Add project files.

---
+++ DataStructures.Trie/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DataStructures.Trie;
+
+
+//Trie trie = new();
+//trie.Add("ali");
+//trie.Add("alp");
+
+//trie.Print();
+
+DenemeTrie denemeTrie = new();
+denemeTrie.Add("ali");
+denemeTrie.Add("alp");
+
+Console.ReadLine();
